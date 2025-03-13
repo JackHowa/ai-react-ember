b11761152f3f980fa60aa87f753aa0880f79276d
Jack Howard <jack.howard@expel.io>
2025-03-13 12:48:44 -0500

    Update to use sonnet 3.5

--- codemods/react-migration/utils/fetch.js
+++ codemods/react-migration/utils/fetch.js
@@ -5,7 +5,7 @@ const { ACCESS_TOKEN, PROJECT_ID } = process.env || {}; // eslint-disable-line
 // claude 3.7 only available in us-east5
 const LOCATION_ID = 'us-east5';
 const API_ENDPOINT = 'us-east5-aiplatform.googleapis.com';
-const MODEL_ID = 'claude-3-7-sonnet@20250219';
+const MODEL_ID = 'claude-3-5-sonnet-v2@20241022';
 const METHOD = 'rawPredict';
 const QUERY_URL = `https://${API_ENDPOINT}/v1/projects/${PROJECT_ID}/locations/${LOCATION_ID}/publishers/anthropic/models/${MODEL_ID}:${METHOD}`;
 
@@ -56,7 +56,7 @@ exports.fetchData = async (inputOrig = {}) => {
 const generatePrompt = ({
   examplesArr = [],
   basePromptStr = 'Convert provided Ember component template Handlebar files to React JSX.',
-  helpfulPromptStr = `Pay attention to these instructions: Do not add 'import React from "react";' for the generated code since React is already defined. The name of the generated React component should be an appropriate unique one (ideally based on the filename and/or parent parent directory comment on the first line if provided) based on the provided input ember code instead of using component names from the provided examples. Do not include "jsx" or "javascript" in the output like \`\`\`jsx or \`\`\`javascript. Do not modify the names of data-* attributes. If you find an uppercase child component like <FancyButton />, try to import it like import FancyButton from '../fancy-button';. Do not try to import like FancyButton from './fancy-button';. Do not explain your role; ONLY give the output of the jsx file.`,
+  helpfulPromptStr = `Pay attention to these instructions: Do not add 'import React from "react";'; there should be no instances of ''import React from "react";' in the output! The name of the generated React component should be an appropriate unique one (ideally based on the filename and/or parent parent directory comment on the first line if provided) based on the provided input ember code instead of using component names from the provided examples. Do not include "jsx" or "javascript" in the output like \`\`\`jsx or \`\`\`javascript. Do not modify the names of data-* attributes. If you find an uppercase child component like <FancyButton />, try to import it like import FancyButton from '../fancy-button';. Do not try to import like FancyButton from './fancy-button';. Do not explain your role; ONLY give the output of the jsx file.`,
   examplePromptStr = `Here are some examples you should follow for best practices which have an Ember template Handlebar code example and its equivalent expected React component:`,
   messagesPromptStr = `Now take your time in generating exactly one React JSX for each provided Ember Handlebar template (from the request messages array field).`,
 } = {}) => {
